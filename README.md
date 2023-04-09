7e4f308bec2ff0bbcb7cd7568c5be601fa97bf2b
Liubov Sobolevskaya <luba@Liubovs-MBP-2.attlocal.net>
2023-04-08 17:41:23 -0700

    Initial solution

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Openweather-Api-Dashboard
 6th assignment of UCB Extension bootcamp
+
+
+[Link to the deployed page](https://liubovsobolevskaya.github.io/Openweather-Api-Dashboard/)
+_______________________________________
+For this assessment we implent a weather website where you can type in a city name, the website will show you the current weather and forecast for that city. It will also keep a history of the cities you've searched for. When you look at the current weather for a city, you'll see the name of the city, the date, and information about the temperature, humidity, and wind speed. When you look at the forecast for a city, you'll see a prediction for the next 5 days, including information about the temperature, wind speed, and humidity. If you want to look at the weather for a city you've already searched for, you can click on it in the search history and it will show you the same information again.
+
+___________________________
+
+## Technology Used 
+
+* [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)      
+* [Git](https://git-scm.com/)   
+* [JavaScript](https://www.javascript.com/)  
+??? or may be not 
+* [Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API) 
+## Web application's appearance and functionality
+
+![](to be added)
+
+## Author Info
+
+### Liubov Sobolevkaya
+* [LinkedIn](https://www.linkedin.com/in/liubov-sobolevskaya-45756a101/)
+* [Github](https://github.com/LiubovSobolevskaya)
+* [Kaggle](https://www.kaggle.com/lyubovsobolevskaya)
---
+++ assets/script.js
@@ -0,0 +1,273 @@
+// Initial array of movies
+
+
+
+var API_key = '6287ac582e2f7161a8095a421dedeb31';
+
+
+
+// displayCityInfo function re-renders the HTML to display the appropriate content
+function displayMainCityInfo() {
+
+
+    
+    //var city = $(this).attr("data-city");
+    var city = $("#city-input").val()
+
+    if (!city){
+       return;
+    }
+    var cityURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_key}`
+    console.log(cityURL)
+    //THEN I am presented with the city name, the date, an icon representation of weather conditions, 
+    // the temperature, the humidity, and the wind speed
+    
+    // Creating an AJAX call for the specific movie button being clicked
+    $.ajax({
+        url: cityURL,
+        method: "GET"
+    }).then(function(response) {
+    
+        console.log(response[0]);
+        var lon = response[0].lon;
+        var lat = response[0].lat;
+  
+        var weatherURL =`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${API_key}`;
+        console.log(weatherURL)
+        
+
+        $.ajax({
+            url: weatherURL,  
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            var temp = response.main.temp;
+            var humidity = response.main.humidity;
+            var windSpeed = response.wind.speed;
+            var icon = response.weather[0].icon;
+            var icon_png = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+            console.log(temp);
+            console.log(humidity);
+            console.log(windSpeed);
+            //console.log(icon );
+            var cityDiv = $('#chosenCity').empty();
+            cityDiv.addClass("card");
+    
+            // Creating an element to have the rating displayed
+            var cityName = $("<h2>");
+            var spanIcon = $("<span>");
+           
+            var weatherIcon = $("<img>");
+            weatherIcon.attr("src", icon_png)
+       
+            cityName.text(`${city} (${dayjs().format("MM/DD/YY")})`);
+            spanIcon.append(weatherIcon);
+            cityName.append(spanIcon);    
+            // Displaying the city and the current Date
+            cityDiv.append(cityName);
+    
+            // Creating an element to hold the release year
+            var pTemp = $("<p>")
+            pTemp.text("Temperature: " + temp + "°F");
+    
+            // Displaying Temperature
+            cityDiv.append(pTemp);
+    
+   
+            var pHumidity = $("<p>")
+            pHumidity.text("Humidity: " + humidity + "%");
+           
+            // Displaying Humidity
+            cityDiv.append(pHumidity);
+
+            // Creating an element to hold the plot
+            var pWindSpeed= $("<p>")
+            pWindSpeed.text("Weend Speed: " + windSpeed + "MPH");
+           
+            // Displaying Wind Speed
+            cityDiv.append(pWindSpeed);
+
+    
+            // Appending new Button
+            var cityButtons = $("#buttons-view");
+            var newCity = $("<button>");
+            newCity.html(city);
+            newCity.attr("data-city", city);
+            newCity.addClass("btn btn-block btn-custom w-90");
+           
+            cityButtons.prepend(newCity);
+        }); 
+        var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${API_key}`;
+        console.log(forecastURL);
+        $.ajax({
+            url: forecastURL,  
+            method: "GET"
+        }).then(function(response) {
+            forecast = response.list;
+         
+
+            var temperature_max = {};
+            var temperature_min= {};
+            var humidity = {};
+            var windSpeed = {};
+            var icon= {};
+            var days = [];
+            for (var i=0;i<forecast.length; i++){
+                
+                var day = forecast[i].dt_txt.split(' ')[0]; 
+                if (!days.includes(day)){
+                    days.push(day);
+
+                }
+               
+                if (!temperature_max[day]){
+                    temperature_max[day] = [];
+                }
+                if (!temperature_min[day]){
+                    temperature_min[day] = [];
+                }
+                if (!humidity[day]){
+                    humidity[day] = [];
+                }
+                if (!windSpeed[day]){
+                    windSpeed[day] = [];
+                }
+                if (!icon[day]){
+                    icon[day] = [];
+                }
+                temperature_max[day].push(forecast[i].main.temp_max);
+                temperature_min[day].push(forecast[i].main.temp_min);
+                humidity[day].push(forecast[i].main.humidity);
+                windSpeed[day].push(forecast[i].wind.speed);
+                icon[day].push(forecast[i].weather[0].icon); 
+
+            }  
+            if (days.length>5){
+                days.shift()
+            }
+
+            console.log(temperature_max);
+            console.log(temperature_min);
+
+            function avg(array) {
+                var sum = 0;
+                for (let i = 0; i < array.length; i++) {
+                    sum += array[i];
+                }
+                return sum / array.length;
+            }
+
+            function max(array) {
+                var max_value = array[0];
+                for (let i = 1; i < array.length; i++) {
+                    if (array[i]>max_value){
+                        max_value = array[i];
+                    }
+                }
+                return max_value;
+            }
+            function min(array) {
+                var min_value = array[0];
+                for (let i = 1; i < array.length; i++) {
+                    if (array[i]<min_value){
+                        min_value = array[i];
+                    }
+                }
+                return min_value;
+            }
+
+
+            console.log(days);
+            moreDaysContainer = $("#more-weather-days").empty();
+            for (var i=0; i<days.length; i++){
+
+                var cityCard = $("<div>");
+                cityCard.addClass("card col-6 col-md-2 col-lg-2 bg-dark text-light");
+                day = days[i];
+                console.log(day);
+  
+                var temp_max = max(temperature_max[day]);
+            
+                var temp_min = min(temperature_min[day]);
+    
+                var humidity_mean = avg(humidity[day]);
+                var windSpeed_max = max(windSpeed[day]);
+               
+                var countIcons = {}
+              
+                for (var j=0; j<icon[day].length; j++){
+                    var icon_img = icon[day][j];
+                    if (!countIcons[icon_img ]){
+                        countIcons[icon_img ] = 1;
+                    }
+                    else{
+                        countIcons[icon_img]++;
+                    }
+                }
+
+                var max_count = countIcons[icon[day][0]];
+                var max_icon = icon[day][0];
+         
+                for (var j=0; j<icon[day].length; j++){
+                    var icon_img = icon[day][j];
+                    if ( countIcons[icon_img] > max_count){
+                        max_count = countIcons[icon_img];
+                        max_icon = icon_img
+                    }
+                }
+
+                var icon_png = `https://openweathermap.org/img/wn/${max_icon}@2x.png`;
+
+                var date = $("<h2>");
+                var spanIcon = $("<span>");
+                var weatherIcon = $("<img>");
+                weatherIcon.attr("src", icon_png)
+
+                date.text(`${day}`);
+                spanIcon.append(weatherIcon);
+                date.append(spanIcon);   
+                cityCard.append(date);
+     
+                var pTemp_min = $("<p>")
+                pTemp_min.text("Temperature min: " + temp_min + "°F");
+                // Displaying  min Temperature
+                cityCard.append(pTemp_min);
+ 
+                var pTemp_max = $("<p>")
+                pTemp_max.text("Temperature max: " + temp_max + "°F");
+                // Displaying  max Temperature
+                cityCard.append(pTemp_max);
+
+                var pHum = $("<p>")
+                pHum.text("Average Humidity: " + humidity_mean + "%");
+            
+                // Displaying Humidity
+                cityCard.append(pHum);
+     
+                // Creating an element to hold the plot
+                var pWindSp= $("<p>")
+                pWindSp.text("Max Wind Speed: " + windSpeed_max + "MPH");
+                // Displaying Wind Speed
+                cityCard.append(pWindSp);
+                moreDaysContainer.append(cityCard);
+
+              
+             
+            }
+   
+
+
+          
+        }); 
+
+
+
+
+
+
+
+    });
+}
+$("#city-search-btn").on("click", displayMainCityInfo);
+
+
